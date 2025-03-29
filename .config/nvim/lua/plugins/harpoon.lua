15821e6f7cc20bb26a489037ee24307df2811c44
LoukasN <lounikus@gmail.com>
2025-03-29 15:26:05 +0200

    moved to harpoon2

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,24 +1,25 @@
 return {
 	'ThePrimeagen/harpoon',
+	branch = 'harpoon2',
 	config = function()
 		require("telescope").load_extension('harpoon')
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-		vim.keymap.set("n", "<leader>ah", mark.add_file, {desc = "Add harpoon"})
-		vim.keymap.set("n", "<leader>ph", ui.toggle_quick_menu, {desc = "Harpoons"})
+		vim.keymap.set("n", "<leader>ah", function() harpoon:list():add() end, {desc = "Add harpoon"})
+		vim.keymap.set("n", "<leader>ph", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "Harpoons"})
 
-		vim.keymap.set("n", "<A-Tab>", ui.nav_next, {desc = "Next harpoon"})
-		vim.keymap.set("n", "<A-S-Tab>", ui.nav_prev, {desc = "Previous harpoon"})
+		vim.keymap.set("n", "<A-Tab>", function() harpoon:list():next() end, {desc = "Next harpoon"})
+		vim.keymap.set("n", "<A-S-Tab>", function() harpoon:list():prev() end, {desc = "Previous harpoon"})
 
-		vim.keymap.set("n", "<A-1>", function() ui.nav_file(1) end)
-		vim.keymap.set("n", "<A-2>", function() ui.nav_file(2) end)
-		vim.keymap.set("n", "<A-3>", function() ui.nav_file(3) end)
-		vim.keymap.set("n", "<A-4>", function() ui.nav_file(4) end)
-		vim.keymap.set("n", "<A-5>", function() ui.nav_file(5) end)
-		vim.keymap.set("n", "<A-6>", function() ui.nav_file(6) end)
-		vim.keymap.set("n", "<A-7>", function() ui.nav_file(7) end)
-		vim.keymap.set("n", "<A-8>", function() ui.nav_file(8) end)
-		vim.keymap.set("n", "<A-9>", function() ui.nav_file(9) end)
+		vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<A-5>", function() harpoon:list():select(5) end)
+		vim.keymap.set("n", "<A-6>", function() harpoon:list():select(6) end)
+		vim.keymap.set("n", "<A-7>", function() harpoon:list():select(7) end)
+		vim.keymap.set("n", "<A-8>", function() harpoon:list():select(8) end)
+		vim.keymap.set("n", "<A-9>", function() harpoon:list():select(9) end)
 	end,
 }
