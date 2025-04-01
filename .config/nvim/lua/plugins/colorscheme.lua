72e81ab55300961f5f2a92462f9094ac9817c8a6
LoukasN <lounikus@gmail.com>
2025-04-01 20:43:06 +0300

    colorscheme change

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,10 @@ return {
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
-		enabled = false,
+		enabled = true,
 		config = function()
 			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				flavour = "macchiato", -- latte, frappe, macchiato, mocha
 				transparent_background = false,
 				integrations = {
 					cmp = true,
@@ -28,13 +28,13 @@ return {
 		config = function()
 			require("nightfox").setup({
 				options = {
-					transparent = true,
+					transparent = false,
 				},
 			})
 			-- vim.cmd.colorscheme("carbonfox")
 			-- vim.cmd.colorscheme("dawnfox")
 			-- vim.cmd.colorscheme("dayfox")
-			vim.cmd.colorscheme("duskfox")
+			-- vim.cmd.colorscheme("duskfox")
 			-- vim.cmd.colorscheme("nightfox")
 			-- vim.cmd.colorscheme("nordfox")
 			-- vim.cmd.colorscheme("terafox")
@@ -44,31 +44,17 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		priority = 1000,
-		enabled = false,
+		enabled = true,
 		config = function()
 			require("tokyonight").setup({
 				-- transparent = true,
 				transparent = false,
-				style = "moon",
-				-- style = "storm",
+				-- style = "moon",
+				style = "storm",
 				-- style = "night",
 				-- style = "day",
 			})
-			vim.cmd.colorscheme "tokyonight"
-		end,
-	},
-
-	{
-		"Mofiqul/dracula.nvim",
-		priortiy = 1000,
-		enabled = false,
-		config = function()
-			require("dracula").setup({
-				transparent_bg = true,
-				-- transparent_bg = false,
-			})
-			vim.cmd.colorscheme("dracula")
-			-- vim.cmd.colorscheme("dracula-soft")
+			-- vim.cmd.colorscheme("tokyonight")
 		end,
 	},
 }
