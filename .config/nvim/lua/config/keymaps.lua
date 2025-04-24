07ae3c4f00453ecb7c0adfde17fca7ea9f5d1486
LoukasN <lounikus@gmail.com>
2025-04-24 22:42:24 +0300

    hide cmd bar

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.o.cmdheight = 0
