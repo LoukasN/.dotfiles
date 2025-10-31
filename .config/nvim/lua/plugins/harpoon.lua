5af742754dd74a5e285a69bcf4104d29416c3a5b
LoukasN <lounikus@gmail.com>
2025-10-31 13:39:43 +0200

    removed telescope dependencie

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,8 +1,8 @@
 return {
 	'ThePrimeagen/harpoon',
 	branch = 'harpoon2',
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("telescope").load_extension('harpoon')
 		local harpoon = require("harpoon")
 		harpoon:setup()
 
