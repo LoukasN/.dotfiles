efd3bb82cdb92b43aa94ef645837cb314dfd5118
LoukasN <lounikus@gmail.com>
2025-10-31 13:40:00 +0200

    fixed indentation

--- .config/rofi/scripts/hypr/rofi-wallpaper-changer-hyprland.sh
+++ .config/rofi/scripts/hypr/rofi-wallpaper-changer-hyprland.sh
@@ -5,18 +5,17 @@ ConfigLocation="/home/$USER/.config/hypr/autostart.conf" # The config file to ed
 PrevWallpaper=$(cat "$ConfigLocation" | grep "exec = swww img " | awk -F '/' '{print $4}') # Get previous wallpapers lines from file)
 
 SelectPic(){
-	# Shows the wallpaper selection menu in rofi
-	Wallpaper=$(ls "$WallpaperDir" | rofi -dmenu -p "Select wallpaper")
-	if [[ $Wallpaper == "q" || $Wallpaper == "" ]]; then
-		exit
-	else
-		pkill swaybg
-		SetWallpaper
-	fi
+    # Shows the wallpaper selection menu in rofi
+    Wallpaper=$(ls "$WallpaperDir" | rofi -dmenu -p "Select wallpaper")
+    if [[ $Wallpaper == "q" || $Wallpaper == "" ]]; then
+        exit
+    else
+        SetWallpaper
+    fi
 }
 
 SetWallpaper(){
-	sed -i -e "s/$PrevWallpaper/$Wallpaper/g" "$ConfigLocation"
+    sed -i -e "s/$PrevWallpaper/$Wallpaper/g" "$ConfigLocation"
 }
 
 SelectPic
