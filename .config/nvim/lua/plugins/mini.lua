e4c5c9950a76dd3b24d30e3c4d972888243b2ef5
LoukasN <lounikus@gmail.com>
2026-05-25 00:18:30 +0300

    removed redundant binds

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,20 +3,7 @@ return {
 	enabled = true,
 	version = false,
 	config = function()
-		require("mini.surround").setup({
-			mappings = {
-				add = "sa", -- Add surrounding in Normal and Visual modes
-				delete = "sd", -- Delete surrounding
-				find = "sf", -- Find surrounding (to the right)
-				find_left = "sF", -- Find surrounding (to the left)
-				highlight = "sh", -- Highlight surrounding
-				replace = "sr", -- Replace surrounding
-				update_n_lines = "sn", -- Update `n_lines`
-
-				suffix_next = "n", -- Suffix to search with "next" method
-				suffix_last = "p", -- Suffix to search with "prev" method
-			},
-		})
+		require("mini.surround").setup({})
 		require("mini.comment").setup({})
 		require("mini.cursorword").setup({})
 	end,
