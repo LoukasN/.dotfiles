fbd4386334675a8008e6af4f4f40e0c30ac82579
LoukasN <lounikus@gmail.com>
2026-07-03 23:36:31 +0300

    joined scripts

--- .local/bin/screenshot-window.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-fileName="$(date +%F_%H-%M-%S)"
-grim -g "$(slurp)" ~/Pictures/screenshots/"$fileName.png"
-wl-copy --type image/png < ~/Pictures/screenshots/"$fileName.png"
-notify-send  'Screenshot captured and copied to clipboard'
--- .local/bin/screenshot.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-fileName="$(date +%F_%H-%M-%S)"
-grim ~/Pictures/screenshots/"$fileName.png"
-wl-copy --type image/png < ~/Pictures/screenshots/"$fileName.png"
-notify-send 'Screen captured and copied to clipboard'
