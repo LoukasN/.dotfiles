ead4fe9b501da13a2d7a75b62716b3379c18987a
LoukasN <lounikus@gmail.com>
2026-09-10 19:21:22 +0300

    Per workspace layout, centering keybind and animation toggle function

--- .config/hypr/binds.lua
+++ .config/hypr/binds.lua
@@ -1,5 +1,21 @@
 require("variables")
 
+-- Per layout binds function
+local function layout_bind(bind_table)
+	return function()
+		local workspace = hl.get_active_special_workspace() or hl.get_active_workspace()
+		if not workspace then
+			return
+		end
+
+		local layout = workspace.tiled_layout
+
+		if bind_table[layout] then
+			hl.dispatch(bind_table[layout])
+		end
+	end
+end
+
 --[[
  ____            _      
 | __ )  __ _ ___(_) ___ 
@@ -14,17 +30,24 @@ hl.bind("SUPER + SHIFT + Q", hl.dsp.window.close())
 hl.bind("SUPER + D", hl.dsp.exec_cmd("rofi -show drun"))
 hl.bind("SUPER + P", hl.dsp.window.pseudo())
 hl.bind("SUPER + F", hl.dsp.window.fullscreen())
--- hl.bind("SUPER + SHIFT + F", hl.dsp.exec_cmd("pkill -SIGUSR1 waybar"))
+hl.bind("SUPER + C", hl.dsp.window.center())
 hl.bind("SUPER + SHIFT + F", hl.dsp.exec_cmd("quickshell ipc call bar toggle"))
--- hl.bind("SUPER + SHIFT + Z", hl.dsp.exec_cmd("pkill waybar || waybar"))
 hl.bind("SUPER + N", hl.dsp.exec_cmd("quickshell ipc call notifications toggle"))
 
-if Layout == "scrolling" then
-	hl.bind("SUPER + SPACE", hl.dsp.layout("consume_or_expel next"))
-	hl.bind("SUPER + SHIFT + SPACE", hl.dsp.window.float({ action = "toggle" }))
-elseif Layout == "dwindle" then
-	hl.bind("SUPER + SPACE", hl.dsp.window.float({ action = "toggle" }))
-end
+hl.bind(
+	"SUPER + SPACE",
+	layout_bind({
+		dwindle = hl.dsp.window.float({ action = "toggle" }),
+		scrolling = hl.dsp.layout("consume_or_expel next"),
+	})
+)
+
+hl.bind(
+	"SUPER + SHIFT + SPACE",
+	layout_bind({
+		scrolling = hl.dsp.window.float({ action = "toggle" }),
+	})
+)
 
 --[[
  ____            _       _       
@@ -35,13 +58,9 @@ end
                   |_|           
 --]]
 
--- hl.bind("SUPER + I", hl.dsp.exec_cmd("bash ~/.config/rofi/scripts/global/rofi-wifi-menu.sh"))
 hl.bind("SUPER + I", hl.dsp.exec_cmd("quickshell ipc call networkMenu toggle"))
--- hl.bind("SUPER + B", hl.dsp.exec_cmd("bash ~/.config/rofi/scripts/global/rofi-bluetooth-menu.sh"))
 hl.bind("SUPER + B", hl.dsp.exec_cmd("quickshell ipc call bluetoothMenu toggle"))
--- hl.bind("SUPER + SHIFT + DELETE", hl.dsp.exec_cmd("bash ~/.config/rofi/scripts/hypr/powermenu.sh"))
 hl.bind("SUPER + SHIFT + DELETE", hl.dsp.exec_cmd("quickshell ipc call shutdownMenu toggle"))
--- hl.bind("SUPER + W", hl.dsp.exec_cmd("bash ~/.config/rofi/scripts/hypr/rofi-wallpaper-changer-hyprland.sh"))
 hl.bind("SUPER + W", hl.dsp.exec_cmd("quickshell ipc call wallpaperMenu toggle"))
 hl.bind("SUPER + SHIFT + W", hl.dsp.exec_cmd("bash ~/WindowsVM/cli-vm.sh"))
 hl.bind("SUPER + V", hl.dsp.exec_cmd("bash ~/.config/rofi/scripts/global/rofi-history-menu.sh"))
@@ -60,29 +79,44 @@ hl.bind("SUPER + L", hl.dsp.focus({ direction = "right" }))
 hl.bind("SUPER + K", hl.dsp.focus({ direction = "up" }))
 hl.bind("SUPER + J", hl.dsp.focus({ direction = "down" }))
 
+hl.bind(
+	"SUPER + SHIFT + H",
+	layout_bind({
+		dwindle = hl.dsp.window.move({ direction = "left" }),
+		scrolling = hl.dsp.layout("swapcol l"),
+	})
+)
+
 -- Move window with SUPER + SHIFT + vim motions
-if Layout == "dwindle" then
-	hl.bind("SUPER + SHIFT + H", hl.dsp.window.move({ direction = "left" }))
-	hl.bind("SUPER + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
-elseif Layout == "scrolling" then
-	hl.bind("SUPER + SHIFT + H", hl.dsp.layout("swapcol l"))
-	hl.bind("SUPER + SHIFT + L", hl.dsp.layout("swapcol r"))
-end
+hl.bind(
+	"SUPER + SHIFT + L",
+	layout_bind({
+		dwindle = hl.dsp.window.move({ direction = "right" }),
+		scrolling = hl.dsp.layout("swapcol r"),
+	})
+)
 hl.bind("SUPER + SHIFT + K", hl.dsp.window.move({ direction = "up" }))
 hl.bind("SUPER + SHIFT + J", hl.dsp.window.move({ direction = "down" }))
 
 -- Resizing
-if Layout == "dwindle" then
-	hl.bind("SUPER + RIGHT", hl.dsp.window.resize({ x = 40, y = 0, relative = true, repeating = true }))
-	hl.bind("SUPER + LEFT", hl.dsp.window.resize({ x = -40, y = 0, relative = true, repeating = true }))
-	hl.bind("SUPER + UP", hl.dsp.window.resize({ x = 0, y = 40, relative = true, repeating = true }))
-	hl.bind("SUPER + DOWN", hl.dsp.window.resize({ x = 0, y = -40, relative = true, repeating = true }))
-elseif Layout == "scrolling" then
-	hl.bind("SUPER + RIGHT", hl.dsp.layout("colresize +conf"))
-	hl.bind("SUPER + LEFT", hl.dsp.layout("colresize -conf"))
-	hl.bind("SUPER + UP", hl.dsp.window.resize({ x = 0, y = 40, relative = true, repeating = true }))
-	hl.bind("SUPER + DOWN", hl.dsp.window.resize({ x = 0, y = -40, relative = true, repeating = true }))
-end
+hl.bind(
+	"SUPER + RIGHT",
+	layout_bind({
+		dwindle = hl.dsp.window.resize({ x = 40, y = 0, relative = true, repeating = true }),
+		scrolling = hl.dsp.layout("colresize +conf"),
+	})
+)
+
+hl.bind(
+	"SUPER + LEFT",
+	layout_bind({
+		dwindle = hl.dsp.window.resize({ x = -40, y = 0, relative = true, repeating = true }),
+		scrolling = hl.dsp.layout("colresize -conf"),
+	})
+)
+
+hl.bind("SUPER + UP", hl.dsp.window.resize({ x = 0, y = 40, relative = true, repeating = true }))
+hl.bind("SUPER + DOWN", hl.dsp.window.resize({ x = 0, y = -40, relative = true, repeating = true }))
 
 -- Mouse Resizing
 hl.bind("SUPER + mouse:272", hl.dsp.window.drag(), { mouse = true })
@@ -107,6 +141,7 @@ end
 hl.bind("SUPER + MINUS", hl.dsp.workspace.toggle_special("scratchpad"))
 hl.bind("SUPER + SHIFT + MINUS", hl.dsp.window.move({ workspace = "special:scratchpad", follow = false }))
 hl.workspace_rule({ workspace = "special:scratchpad", persistent = true })
+hl.workspace_rule({ workspace = "special:scratchpad", layout = "scrolling" })
 
 -- Music special workspace
 hl.bind("SUPER + M", hl.dsp.workspace.toggle_special("media"))
@@ -161,3 +196,24 @@ hl.bind("SHIFT + PRINT", hl.dsp.exec_cmd("bash ~/.local/bin/screenshot select"),
 
 -- Lock on lid close
 hl.bind("switch:on:Lid", hl.dsp.exec_cmd("systemctl suspend"))
+
+-- Powersave toggle function
+hl.bind("SUPER + F12", function()
+	local powersave = (hl.get_config("animations.enabled") == false)
+	if powersave then
+		hl.exec_cmd("hyprctl reload")
+		hl.exec_cmd('notify-send "Animations enabled"')
+		return
+	else
+		hl.exec_cmd('notify-send "Animations disabled"')
+	end
+	hl.config({
+		animations = {
+			enabled = false,
+		},
+		decoration = {
+			shadow = { enabled = false },
+			blur = { enabled = false },
+		},
+	})
+end)
